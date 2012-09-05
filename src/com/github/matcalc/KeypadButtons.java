19cbb6d0fda03ce7e7a71df346df725007b8fbce
Kelong Cong <kelongcong@gmail.com>
2012-09-06 00:53:32 +1200

    added jama library, implemented four basic operations, implemented decimal parsing

--- src/com/github/matcalc/KeypadButtons.java
+++ src/com/github/matcalc/KeypadButtons.java
@@ -27,5 +27,4 @@ public enum KeypadButtons {
 		return mText;
 	}
 	
-
 }
--- src/com/github/matcalc/MainActivity.java
+++ src/com/github/matcalc/MainActivity.java
@@ -1,6 +1,9 @@
 package com.github.matcalc;
 
+import Jama.Matrix;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -8,6 +11,7 @@ import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.Toast;
 
@@ -53,17 +57,80 @@ public class MainActivity extends Activity {
 	}
 
 	protected void ProcessKeypadInput(KeypadButtons keypadButton) {
-		// TODO Auto-generated method stub
-		Toast.makeText(
-				MainActivity.this,
-				keypadButton.getText().toString() + " "
-						+ keypadButton.toString(), Toast.LENGTH_SHORT).show();
-//		switch (keypadButton) {
-//		case ADD:
-//			Toast.makeText(MainActivity.this, "add", Toast.LENGTH_SHORT).show();
-//		default:
-//			Toast.makeText(MainActivity.this, "default", Toast.LENGTH_SHORT).show();
-//		}
+
+		EditText matrixA = (EditText) findViewById(R.id.matrixA);
+		EditText matrixB = (EditText) findViewById(R.id.matrixB);
+
+		String strMatrixA = matrixA.getText().toString();
+		String strMatrixB = matrixB.getText().toString();
+
+		double[][] dblMatrixA = MatrixParser.parse(strMatrixA);
+		double[][] dblMatrixB = MatrixParser.parse(strMatrixB);
+
+		Matrix A = new Matrix(dblMatrixA);
+		Matrix b = new Matrix(dblMatrixB);
+		Matrix x;
+
+		double[][] dblAns;
+		int row;
+		int col;
+		String ans;
+
+		// switch between different button pressed
+		switch (keypadButton) {
+		case ADD:
+			x = A.plus(b);
+			break;
+
+		case SUBTRACT:
+			x = A.minus(b);
+			break;
+
+		case MULTIPLY:
+			x = A.times(b);
+			break;
+
+		case DIVIDE:
+			x = b.solve(A);
+			break;
+
+		default:
+			System.out.println(keypadButton.getText().toString() + " "
+					+ keypadButton.toString());
+			x = null;
+			break;
+		}
+		dblAns = x.getArrayCopy();
+		row = x.getRowDimension();
+		col = x.getColumnDimension();
+		ans = MatrixParser.dblToStr(dblAns, row, col);
+
+		// Toast to display which button is pressed
+		Toast toast = Toast
+				.makeText(MainActivity.this, keypadButton.getText().toString()
+						+ " " + keypadButton.toString(), Toast.LENGTH_SHORT);
+
+		// Dialog to show the results and additional options
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Output:")
+				.setMessage(ans)
+				.setCancelable(false)
+				.setPositiveButton("Yes",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								MainActivity.this.finish();
+							}
+						})
+				.setNegativeButton("No", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+		AlertDialog alert = builder.create();
+
+		// Showing dialog and toast
+		toast.show();
+		alert.show();
 
 	}
 
